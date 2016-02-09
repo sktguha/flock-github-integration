760ea5bd7590a1ec1b5c028e501ddee8a02ddfa2
test <test@gmail.com>
2016-02-09 21:03:41 +0530

    add ending request

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var http = require('http');
 http.createServer( function(req, res) {
     console.log(req.url);
+    res.end('please visit https://github.com/sktguha/bluff/');
 }).listen(process.env.PORT || 8000);
