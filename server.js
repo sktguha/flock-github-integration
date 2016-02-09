33a1e705b7871ffe5e4de845f6a3c241530a1113
test <test@gmail.com>
2016-02-10 01:40:46 +0530

    add procfile and modify server

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 var http = require('http');
 http.createServer( function(req, res) {
     console.log(req.url);
-    res.end('please visit https://github.com/sktguha/bluff/');
+    var body = "";
+    req.on('data', function (chunk) {
+    	body += chunk;
+  	});
+  req.on('end', function () {
+    console.log('body: ' + body);
+    var jsonObj = JSON.parse(body);
+  })
+    res.end('please visit https://github.com/sktguha/flock-github-integration', 200);
 }).listen(process.env.PORT || 8000);
