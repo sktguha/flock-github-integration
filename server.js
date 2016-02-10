6fd3ed838bc9b8243d252499e384ecc68f1ec7a3
test <test@gmail.com>
2016-02-10 16:57:57 +0530

    added better logging

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var http = require('http');
+var request = require('request');
+var _ = require('underscore');
 var name = '@sktguha';
 var flockToken = process.env.FLOCK_TOKEN || (process.argv && process.argv[2]);
 var commentUrl = 'http://www.google.com';
@@ -16,7 +18,8 @@ var commentUrl = 'http://www.google.com';
     		}
     		}); */
 http.createServer( function(req, res) {
-    console.log(req.url);
+    res.uniqueCustomTimestamp = Date.now();
+    console.log('url : ',req.url, ' id : '+res.uniqueCustomTimestamp);
     var payload = "";
     req.on('data', function (chunk) {
     	payload += chunk;
@@ -25,18 +28,17 @@ http.createServer( function(req, res) {
     try{
     console.log('payload: ' + payload);
     payload = JSON.parse(payload);
-    var body = payload.comment.body;
-    var commentUrl = payload.comment.html_url;
-    console.log('body and username : ', name , body);
+    console.log('body and username : ', body, name );
+   var body = payload.comment.body;
     if (body.indexOf(name) !==-1){
     	//make the flock url call
     	var repoName = payload.repository.name;
     	var userName = payload.comment.user.login;
-    	var commentSrc = payload.comment.html_url;
-    	sendToFlock({
-    		text : userName+'('+repoName+') : '+body,
-    	});
-    	sendToFlock({
+        var commentUrl = payload.comment.html_url;
+        sendToFlock({
+    		text : userName+'('+repoName+') : '+body
+    	}, res);
+    	/*sendToFlock({
     		content : {
     			mime_type: "text/html",
     			source : commentUrl ,
@@ -45,19 +47,23 @@ http.createServer( function(req, res) {
     			 	mime_type: "text/html"
     			 }]
 			}
-		});
+		}, res);
+		*/
+    }  else {
+        res.end('username not found in body' +  body + ' name : '+name+' id : '+res.uniqueCustomTimestamp, 200);
     }
 	} catch(e){
 		console.error(e);
+		res.end('error occurred. ' + e.toString()+ ' id '+res.uniqueCustomTimestamp, 500)
 	}
   })
-    res.end('please visit https://github.com/sktguha/flock-github-integration', 200);
+    
 }).listen(process.env.PORT || 8000);
 
-function sendToFlock(data){
+function sendToFlock(data, res){
 	var url = 'https://api.flock.co/hooks/sendMessage/'+ flockToken;
-	console.log(url);
-	require('request')({
+	console.log('url and data ',url, data);
+	request({
     uri: url,
     method: "POST",
     json: true,
@@ -65,14 +71,17 @@ function sendToFlock(data){
         "content-type": "application/json",
     },
     body: data
-	}, reqCallback);
-	console.log('request sent to flock');
+	}, _.partial(reqCallback, [data, res]));
+	console.log('call to flock issued. parameters : ', data);
 }
 
-function reqCallback(error, response, body) {
- 	if(error){
+function reqCallback(args, error, response, body) {
+ 	var data = args[0], res = args[1];
+    if(error){
  		console.error('error occurred', error);
+ 		res.end('error occurred. ' + error.toString()+ ' data : '+data+" id : "+res.uniqueCustomTimestamp, 500);
  		return;
  	} 
   console.log('response' , response.body);
+  res.end('request sent to flock. ' + JSON.stringify(response.body)+' data : '+JSON.stringify(data)+' id : '+res.uniqueCustomTimestamp, 200);
 }
